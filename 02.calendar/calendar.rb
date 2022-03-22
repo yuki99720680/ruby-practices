9d48dc1eae70ab4d62e8640c24e5a620337c7d09
m-kimura <mk.alstroemeria@gmail.com>
2022-03-22 15:11:41 +0900

    [clean] dateオブジェクトを活用する形に変更

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,54 +3,35 @@
 require 'date'
 require 'optparse'
 
-def generate_days_list(year, month)
+def generate_dates_list(year, month)
   current_day = 1
   last_day = Date.new(year, month, -1).day
-  days = []
+  dates = []
 
   while current_day <= last_day
     date = Date.new(year, month, current_day)
-    if current_day < 10
-      day = format("% 2d", current_day)
-    else
-      day = current_day.to_s
-    end
-    day_of_week = date.strftime('%a')
-    days << [day, day_of_week]
+    dates << date
     current_day += 1
   end
 
-  days
+  dates
 end
 
-def console_out(days, date, today)
-  puts "      #{date.month}月 #{date.year}"
+def console_out(dates, today)
+  puts "      #{dates[0].month}月 #{dates[0].year}"
   puts "日 月 火 水 木 金 土"
 
   space = "   "
 
-  case days[0][1]
-  when "Mon"
-    print space 
-  when "Tue"
-    print space * 2 
-  when "Wed"
-    print space * 3 
-  when "Thu"
-    print space * 4 
-  when "Fri"
-    print space * 5 
-  when "Sat"
-    print space * 6 
-  end
+  print space * dates[0].wday
   
-  days.each do |day, day_of_week|
-    if date.year == today.year && date.month == today.month && day == today.day.to_s
-      print "\e[7m" + days[i][0] + "\e[0m" + " "
+  dates.each do |date|
+    if date == today
+      print "\e[7m#{date.day}\e[0m "
     else
-      print day + " "
+      print "#{date.day.to_s.rjust(2)} "
     end
-    if day_of_week == "Sat"
+    if date.saturday?
       puts ""
     end
   end
@@ -69,5 +50,5 @@ opt.parse(ARGV)
 
 date = Date.new(year, month, 1)
 
-days = generate_days_list(year, month)
-console_out(days, date, today)
+dates = generate_dates_list(year, month)
+console_out(dates, today)
