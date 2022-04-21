fd274a21c0a8ccfa56bda53d21e6819e867e47c8
m-kimura <mk.alstroemeria@gmail.com>
2022-04-21 10:28:26 +0900

    [clean] メソッド化、変数名変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -38,9 +38,9 @@ def main
   return unless files
 
   if long_format_flag
-    long_format_files = build_file_stats(files) # todo 名前が微妙=>file_stats
-    total_block_size = calculate_total_block_size(long_format_files)
-    l_option_output(total_block_size, long_format_files)
+    file_stats = build_file_stats(files)
+    total_block_size = calculate_total_block_size(file_stats)
+    l_option_output(total_block_size, file_stats)
   else
     basename_files = basename_files(files)
     padded_files = add_padding(basename_files)
@@ -49,33 +49,33 @@ def main
 end
 
 def enumerate_files
-  path = generate_base_directory
-  if Dir.exist?(path)
-    Dir.glob("#{path}*")
+  base_directory = generate_base_directory
+  if Dir.exist?(base_directory)
+    Dir.glob("#{base_directory}*")
   else
-    puts "ls: #{path}: No such file or directory"
+    puts "ls: #{base_directory}: No such file or directory"
   end
 end
 
 def build_file_stats(files)
-  long_format_files = [] # todo 名前が微妙=>file_stats
+  file_stats = []
   files.each do |file| # todo mapに置き換える
-    stat_raw = File.symlink?(file) ? File.lstat(file) : File.stat(file) # todo 名前が微妙 stat_raw=>
-    stat = {}
-    stat[:mode] = generate_mode(stat_raw)
-    stat[:nlink] = stat_raw.nlink
-    stat[:uid] = Etc.getpwuid(stat_raw.uid).name
-    stat[:gid] = Etc.getgrgid(stat_raw.gid).name
-    stat[:size] = stat_raw.size
-    stat[:block] = stat_raw.blocks
-    stat[:mtime] = stat_raw
-    stat[:name] = File.basename(file)
-    path = generate_base_directory
-    file_path = "#{path}#{stat[:name]}" # todo メソッド化 generate_file_path
-    stat[:symlink] = File.readlink(file_path) if File.symlink?(file_path)
-    long_format_files << stat
+    stat = File.symlink?(file) ? File.lstat(file) : File.stat(file)
+    file_info = {}
+    file_info[:mode] = generate_mode(stat)
+    file_info[:nlink] = stat.nlink
+    file_info[:uid] = Etc.getpwuid(stat.uid).name
+    file_info[:gid] = Etc.getgrgid(stat.gid).name
+    file_info[:size] = stat.size
+    file_info[:block] = stat.blocks
+    file_info[:mtime] = stat
+    file_info[:name] = File.basename(file)
+    base_directory = generate_base_directory
+    file_path = generate_file_path(base_directory, file_info[:name])
+    file_info[:symlink] = File.readlink(file_path) if File.symlink?(file_path)
+    file_stats << file_info
   end
-  long_format_files
+  file_stats
 end
 
 def generate_mode(stat_raw)
@@ -101,9 +101,9 @@ end
 
 def calculate_total_block_size(long_format_files)
   total_block_size = 0
-  path = generate_base_directory
+  base_directory = generate_base_directory
   long_format_files.each do |file| # todo sumメソッドに置き換え（条件に合致しないとき(するとき)は0を返す)
-    file_path = "#{path}#{file[:name]}" # todo メソッド化 generate_file_path
+    file_path = generate_file_path(base_directory, file[:name])
     total_block_size += file[:block] if File.file?(file_path) && !File.symlink?(file_path)
   end
   total_block_size
@@ -113,21 +113,25 @@ def generate_base_directory
   ARGV[0] || './'
 end
 
+def generate_file_path(base_directory, file_name)
+  "#{base_directory}#{file_name}"
+end
+
 def l_option_output(total_block_size, long_format_files)
   nlink_padding, uid_paddinng, gid_paddinng = calculate_padding_size(long_format_files)
   puts "total #{total_block_size}"
   long_format_files.each do |file| # todo mapに置き換える
-    stat = {}
-    stat[:mode] = file[:mode].to_s.ljust(10)
-    stat[:nlink] = file[:nlink].to_s.rjust(nlink_padding)
-    stat[:uid] = file[:uid].to_s.rjust(uid_paddinng)
-    stat[:gid] = file[:gid].to_s.rjust(gid_paddinng)
-    stat[:size] = file[:size].to_s.rjust(6)
-    stat[:mtime] = file[:mtime].mtime.strftime(' %_m %e %H:%M').to_s
-    stat[:name] = " #{file[:name]}"
-    stat[:symlink] = " -> #{file[:symlink]}" if file[:symlink]
-    stat.each_value do |s|
-      print s
+    file_info = {}
+    file_info[:mode] = file[:mode].to_s.ljust(10)
+    file_info[:nlink] = file[:nlink].to_s.rjust(nlink_padding)
+    file_info[:uid] = file[:uid].to_s.rjust(uid_paddinng)
+    file_info[:gid] = file[:gid].to_s.rjust(gid_paddinng)
+    file_info[:size] = file[:size].to_s.rjust(6)
+    file_info[:mtime] = file[:mtime].mtime.strftime(' %_m %e %H:%M').to_s
+    file_info[:name] = " #{file[:name]}"
+    file_info[:symlink] = " -> #{file[:symlink]}" if file[:symlink]
+    file_info.each_value do |f|
+      print f
     end
     puts ''
   end
